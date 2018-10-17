141dfc4dc6c8d3b4501ca426bc3bfb2288f415f1
Flaurenss <42294298+Flaurenss@users.noreply.github.com>
2018-10-17 20:09:48 +0200

    Add correct functionality to piano notes

---
+++ GameProject/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Click()
+    {
+        GetComponent<Image>().color = Color.gray;
+        //Notes should be played here
+        GetComponent<AudioSource>().Play();
+    }
+    //Start Same functions
+    public void ClickUp()
+    {
+        if (transform.parent.name == "Tonos")
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.black;
+        }
+        
+    }
+
+    public void OnClickExit()
+    {
+        if (transform.parent.name == "Tonos")
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.black;
+        }
+        
+    }
+    //End from same functions
+
+    //This function will be called when the user pass the mouse over the key and after having clicked
+    public void OnClickEnter()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            GetComponent<Image>().color = Color.gray;
+            //Notes should be played here
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
