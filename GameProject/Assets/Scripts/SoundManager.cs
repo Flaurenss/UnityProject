dde7ad62d326ae83a3ca8f6bc083c2b18ac8f29e
Flaurenss <42294298+Flaurenss@users.noreply.github.com>
2018-10-23 20:11:30 +0200

    Add correct semitones and erase duplicated code

--- GameProject/Assets/Scripts/SoundManager.cs
+++ GameProject/Assets/Scripts/SoundManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
-    public static string PianoMode = "cat";
+    public static string PianoMode = "normal";
 
     // Use this for initialization
     void Start ()
